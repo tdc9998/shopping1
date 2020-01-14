7e3048a38515e11d85b77f38def81ce06eb91d58
Tim Crowley <tcrowley@us.ibm.com>
2020-01-14 09:02:57 -0600

    Created a default basic page

--- client/src/components/UIShellBody.jsx
+++ client/src/components/UIShellBody.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import SimpleList from "../pattern-components/SimpleList";
+import BasicPage from "../pattern-components/BasicPage";
 import "../pattern-components/patterns.scss";
 
 class UIShellBody extends Component {
   components = {
-    "Simple List": SimpleList
+    "Simple List": SimpleList,
+    "Basic Page": BasicPage
   };
-  defaultComponent = "Simple List";
+  defaultComponent = "Basic Page";
 
   render() {
     let curScreen = this.defaultComponent;
